81c5681f5895105e1407a0f9c8edc26b70a94454
FlaviaRamosdaSilva <didi.flavia@gmail.com>
2023-06-14 23:36:51 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function generateNumber()
- const min = document.querySelector(".input-min").value
- const max = document.querySelector(".input-max").value
 
- 
+
+function generateNumber() {
+const min = Math.ceil(document.querySelector(".input-min").value)
+const max = Math.floor(document.querySelector(".input-max").value)
+const answer = document.querySelector(".result")
+const disappear = document.querySelector(".text-add")
+
+const result = Math.floor(Math.random() * (max - min + 1)) + min;
+  
+  answer.innerHTML = "Número Sorteado foi " + result;
+
+  disappear.innerHTML = ""
+}
+
+function out() {
+    location.reload();
+}
